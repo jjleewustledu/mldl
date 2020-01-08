c14e0870d5b9e0c8fb4a3a00b06750463b7f14c3
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-01-08 12:50:26 -0600

    [bold,mask] := noise_injection() for downstream use of mask (single 3D)

--- no_package/noise_injector.m
+++ no_package/noise_injector.m
@@ -1,4 +1,4 @@
-function bold = noise_injector(bold, varargin)
+function [bold,mask] = noise_injector(bold, varargin)
     %% NOISE_INJECTOR accepts BOLD time-series data and returns that data injected with modelled noise.
     %  The size of the BOLD data is preserved.   noise_injector may be applied repeatedly.
     %
@@ -7,13 +7,17 @@ function bold = noise_injector(bold, varargin)
     %               is cell of char model specifications.  Default is NoiseInjector.DEFAULT_MODEL.
     %  @param focus_radius is numeric:  default 8.
     %  @param mix is in [0, 1] and determines variability amongst focus voxels.
+    %  @param mix_process is in [0, 1] and determines mix of normal and stochastic process.
+    %  @return bold has noise injected
+    %  @return mask has the randomly defined focus/ball of voxels to be perturbed.
     %
-    %  E.g., bold = noise_injector()
+    %  E.g., bold = noise_injector(bold)
     %  E.g., bold = noise_injector(bold, 'normal')
-    %  E.g., bold = noise_injector(bold, {'Brownian' 'affine'})
+    %  E.g., bold = noise_injector(bold, {'Brownian' 'affine'}, 'mix', 0.5)
     
     NI = NoiseInjector(bold, varargin{:});
     NI = NI.inject_noise_model();
     bold = NI.bold_;
+    mask = NI.focus_;
 end
    
